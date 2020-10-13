fb4f4e0b81ac67f0a3ae07d50ca89e56ce683ae6
Egami-yuk <e.y062097@gmail.com>
2020-10-13 19:21:18 +0900

    チャットルーム名の表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,24 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new
+    @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    # チャットルームで紐付いたメッセージを生成する
+    
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      @messages = @room.messages.includes(:user)
+      render :index
+    end
+  end
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+    # パラメーターの中にログインしているユーザーのidを結合させてcontentを受け取れるようにする
   end
 end
