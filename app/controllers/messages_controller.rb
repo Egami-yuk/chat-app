2c4b61764de88ee8b28aff4089ee98ad081570f9
Egami-yuk <e.y062097@gmail.com>
2020-10-14 12:44:26 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -19,6 +19,7 @@ class MessagesController < ApplicationController
   end
 
   def message_params
+    
     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
     # パラメーターの中にログインしているユーザーのidを結合させてcontentを受け取れるようにする
   end
